93144e8f9fac85d15298e4b1578729d458ce0112
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-04-23 20:19:56 -0300

    Update .gitignore

--- src/polymorphism/polymorphism.java
+++ src/polymorphism/polymorphism.java
@@ -2,6 +2,8 @@ package polymorphism;
 
 public class polymorphism {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    int a = 2;
+
+    System.out.println("a = " + a);
   }
 }
