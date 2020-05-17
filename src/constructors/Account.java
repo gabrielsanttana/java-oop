58f874f5ce4311bf1c509f48ce4ca7d4ff2b382e
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-16 21:51:33 -0300

    Create getter methods

--- src/constructors/Account.java
+++ src/constructors/Account.java
@@ -1,5 +1,29 @@
 package constructors;
 
 public class Account {
-  
+  public int accountNumber;
+  public int balance;
+  public String costumerName;
+  public String costumerEmailAddress;
+  public String costumerPhoneNumber;
+
+  public int getAccountNumber() {
+    return this.accountNumber;
+  }
+
+  public int getBalance() {
+    return this.balance;
+  }
+
+  public String getCostumerName() {
+    return this.costumerName;
+  }
+
+  public String getCostumerEmailAddress() {
+    return this.costumerEmailAddress;
+  }
+
+  public String getCostumerPhoneNumber() {
+    return this.costumerPhoneNumber;
+  }
 }
