270638ec840a7c6782ce2a591f7e98249c991eda
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-05-16 21:57:01 -0300

    Create account class setter methods

--- src/constructors/Account.java
+++ src/constructors/Account.java
@@ -11,19 +11,39 @@ public class Account {
     return this.accountNumber;
   }
 
+  public void setAccountNumber(int accountNumber) {
+    this.accountNumber = accountNumber;
+  }
+
   public int getBalance() {
     return this.balance;
+  }
+
+  public void setBalance(int balance) {
+    this.balance = balance;
   }
 
   public String getCostumerName() {
     return this.costumerName;
   }
 
+  public void setCostumerName(String name) {
+    this.costumerName = name;
+  }
+
   public String getCostumerEmailAddress() {
     return this.costumerEmailAddress;
   }
 
+  public void setCostumerEmailAddress(String emailAddress) {
+    this.costumerEmailAddress = emailAddress;
+  }
+
   public String getCostumerPhoneNumber() {
     return this.costumerPhoneNumber;
+  }
+
+  public void setCostumerPhoneNumber(String phoneNumber) {
+    this.costumerPhoneNumber = phoneNumber;
   }
 }
